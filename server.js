98575a628ba1b416d1a8d1caff03340e97976ac3
AlanProgrammer93 <alanwebprogrammer@gmail.com>
2022-01-20 09:09:43 -0300

    fix reload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path')
 
 const connectDb = require('./config/connectDB');
 connectDb()
@@ -10,7 +11,11 @@ const app = express();
 // Config Use
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public') );
+app.use(express.static('public'));
+
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
